9a5ab18071d0a0d12857ebb5df2846025d65c86c
Loredana Tataru <64546412+lmtataru@users.noreply.github.com>
2020-10-05 10:33:34 +0200

    Add files

---
+++ jquery-ajax/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+	$("#jokes").click(function (e){
+		e.preventDefault();
+		$.ajax({
+			url:`http://api.icndb.com/jokes/random`,
+			type:`get`,
+			dataType:`json`,
+			success: function(data){
+				$("#jokeData").html(data.value.joke);
+			},
+			error: function(xhr, status, error){
+				console.log(xhr);
+				console.log(status);
+				console.log(error);
+			}
+		});
+	});
+});
